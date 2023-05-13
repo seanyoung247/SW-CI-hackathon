3364d59c1132ed73f61ff1a377b9153c8ae6da8b
sean <seanyoung247@gmail.com>
2023-05-13 15:46:34 +0100

    feat: Adds initial chat functionality

--- utils.py
+++ utils.py
@@ -2,8 +2,22 @@ from flask_socketio import SocketIO, emit, join_room, leave_room, rooms
 
 
 def set_arena(player, arena):
-    if player['arena']:
+    if player['arena'] and player['arena'] != player['id']:
+        emit('chat-msg', {
+            'username': player['username'],
+            'data': 'Has Left',
+        }, to=arena, broadcast=True)
         leave_room(player['arena'])
     
     player['arena'] = arena
     join_room(player['arena'])
+    print(player, player['arena'], player['username'])
+    emit('chat-msg', {
+        'username': player['username'],
+        'data': 'Has joined',
+    }, to=player['arena'], broadcast=True)
+
+
+
+def set_challenger(player, challenger):
+    pass
