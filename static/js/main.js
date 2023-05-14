60ba820cc139833b40b488c1536415729bff322f
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-05-14 14:16:56 +0000

    add a modal for select user and game

--- static/js/main.js
+++ static/js/main.js
@@ -40,10 +40,17 @@ import {
     document.getElementById('set-user').addEventListener('click', e => {
         const usernameModal = document.getElementById('username-modal');
         username = document.getElementById('username').value;
+        const characterModal = document.getElementById('character-modal');
+        const gameModal = document.getElementById('game-modal');
+
         if (username) {
             setUsername(username);
             usernameModal.show = false;
+            characterModal.show = true;
+            gameModal.show = true;
         }
+
+        
     });
 
     /*
