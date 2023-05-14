3f9debc2eb6f7ea56cf77bd75935433b07badc8f
sean <seanyoung247@gmail.com>
2023-05-14 14:27:58 +0100

    feat: Initial stat sheet code

--- game.py
+++ game.py
@@ -1,14 +1,40 @@
 
+from random import randint
+from defs import CHARACTERS, WEAPONS, MODIFIERS
 
-# Character, weapon and modifier lists here.
+
+# Stat Sheet:
+# stats = {
+#   player:         player unique id
+#   strength:       calculated strength
+#   skill:          calculated skill
+#   agility:        calculated agility
+#   modifiers: [
+#       {
+#           stat:   The player stat to modify
+#           value:  Modification percentage (0 - 1)
+#       },
+#       {
+#           stat:   The challenger stat to modify
+#           value:  Modification percentage (0 - 1)
+#       }
+#   ]
+# }
 
 
-# This function should take a character, a weapon and a modifier
-# and return a final stat set
-def calc_values(character, weapon, modifier):
+def create_stat_sheet(player, character, weapon, modifer):
+    """ 
+    Creates the final round stat and modifier stat to be sent to resolver function 
+    """
     pass
 
 
-# Takes player 1 and 2 stats and returns the battle results
 def resolve_round(player1, player2):
+    """
+    Takes player 1 and 2 stats and returns the battle results
+    """
+    pass
+
+
+def check_victory(round_result):
     pass
